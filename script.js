115fcf7f27c17c1391121b729a29d48eabe477b1
“gabrielcosta” <onlygcr@gmail.com>
2025-07-25 14:58:32 -0300

    começo do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //tag img - troca de imagem
+  //const img = document.querySelector("#profile img")
+  /*if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/eu-light.PNG")
+    img.setAttribute("alt", "Foto do Gabriel em um dia nublado")
+  } else {
+    img.setAttribute("src", "./assets/eu.jpg")
+  }*/
+}
